191d3bbcc3a399441ca65862a3960349f6f606cc
marsdenmadoka <marsmadoka98@gmail.com>
2024-01-16 18:05:36 +0300

    added a composable

---
+++ app/src/main/java/com/example/newsapp/Presentation/Dimes.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.Presentation
+
+import androidx.compose.ui.unit.dp
+
+object Dimes {
+    val MediumPadding1 = 24.dp
+    val MediumPadding2 = 30.dp
+}
--- app/src/main/java/com/example/newsapp/Presentation/onboarding/components/OnboardingPage.kt
+++ app/src/main/java/com/example/newsapp/Presentation/onboarding/components/OnboardingPage.kt
@@ -1,4 +1,67 @@
 package com.example.newsapp.Presentation.onboarding.components
 
-class OnboardingPage {
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.newsapp.Presentation.Dimes.MediumPadding1
+import com.example.newsapp.Presentation.Dimes.MediumPadding2
+import com.example.newsapp.Presentation.onboarding.Page
+import com.example.newsapp.Presentation.onboarding.pages
+import com.example.newsapp.R
+import com.example.newsapp.ui.theme.NewsAppTheme
+
+@Composable
+fun OnboardingPage(
+    modifier: Modifier = Modifier,
+    page: Page //fetched our page file
+) {
+    Column(modifier = Modifier) {
+        Image(
+            modifier = Modifier
+                .fillMaxSize()
+                .fillMaxSize(fraction = 0.6f),
+            painter = painterResource(id = page.image),
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+        Spacer(modifier = Modifier.height(MediumPadding1))
+
+        Text(
+            text = page.title,
+            modifier = Modifier.padding(horizontal = MediumPadding2),
+            style = MaterialTheme.typography.displaySmall.copy(fontWeight = FontWeight.Bold),
+            color = colorResource(id = R.color.display_small)
+        )
+        Text(
+            text = page.description,
+            modifier = Modifier.padding(horizontal = MediumPadding2),
+            style = MaterialTheme.typography.bodyMedium,
+            color = colorResource(id = R.color.text_medium)
+        )
+
+    }
+
+}
+
+
+
+
+@Preview
+@Composable
+fun OnboardingPagePrevew(){
+    NewsAppTheme {
+        OnboardingPage(page = pages[0])
+    }
 }
