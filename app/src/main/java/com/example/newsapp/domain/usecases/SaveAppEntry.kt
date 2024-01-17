273ffa51c74274d50508322fb7cc9eba246e6c69
marsdenmadoka <marsmadoka98@gmail.com>
2024-01-17 18:52:16 +0300

    adding use cases

--- app/src/main/java/com/example/newsapp/domain/usecases/SaveAppEntry.kt
+++ app/src/main/java/com/example/newsapp/domain/usecases/SaveAppEntry.kt
@@ -1,6 +1,12 @@
 package com.example.newsapp.domain.usecases
 
+import com.example.newsapp.domain.manger.LocalUserManger
+
 class SaveAppEntry(
-    private val localUserManger
+    private val localUserManger:LocalUserManger
 ) {
+
+    suspend operator fun invoke(){
+        localUserManger.saveAppEntry()
+    }
 }
