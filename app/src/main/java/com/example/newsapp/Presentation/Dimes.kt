bb830b8f74870377e6b61876a30b571b1e2a03a3
marsdenmadoka <marsmadoka98@gmail.com>
2024-01-25 15:11:29 +0300

    added the indicator

--- app/src/main/java/com/example/newsapp/Presentation/Dimes.kt
+++ app/src/main/java/com/example/newsapp/Presentation/Dimes.kt
@@ -6,4 +6,5 @@ import androidx.compose.ui.unit.dp
 object Dimes {
     val MediumPadding1 = 24.dp
     val MediumPadding2 = 30.dp
+    val PageIndicatorWidth = 52.dp
 }
--- app/src/main/java/com/example/newsapp/Presentation/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/com/example/newsapp/Presentation/onboarding/OnBoardingScreen.kt
@@ -1,15 +1,26 @@
 package com.example.newsapp.Presentation.onboarding
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.example.newsapp.Presentation.Dimes.MediumPadding2
+import com.example.newsapp.Presentation.Dimes.PageIndicatorWidth
 import com.example.newsapp.Presentation.onboarding.components.OnboardingPage
+import com.example.newsapp.Presentation.onboarding.components.PageIndicator
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -17,29 +28,43 @@ import com.example.newsapp.Presentation.onboarding.components.OnboardingPage
 fun OnBoardingScreen() {
     Column(modifier = Modifier.fillMaxSize()) {//paging
         val pagerState = rememberPagerState(
-            initialPage = 0){
+            initialPage = 0
+        ) {
             pages.size
+        }
+
+        HorizontalPager(state = pagerState) { index ->
+            OnboardingPage(page = pages[index])
+
+        }
+
+        //bottom section for the indicator and button
+        Spacer(modifier = Modifier.weight(1f))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = MediumPadding2)
+                .navigationBarsPadding(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            PageIndicator(modifier = Modifier.width(PageIndicatorWidth), pageSize = pages.size, selectedPage = pagerState.currentPage)
+
         }
 
 
         val buttonState = remember {
             derivedStateOf {
-                when(pagerState.currentPage){
-                    0 -> listOf("","Next")
-                    1 -> listOf("Back","Next")
-                    2 -> listOf("Back","Get Started")
+                when (pagerState.currentPage) {
+                    0 -> listOf("", "Next")
+                    1 -> listOf("Back", "Next")
+                    2 -> listOf("Back", "Get Started")
                     else -> listOf("")
 
                 }
             }
         }
-        
-        
-        HorizontalPager(state = pagerState) { index->
-            OnboardingPage(page = pages[index])
-            
-        }
-        
+
 
     }
 }
