b2433c6aa2c38d48a23617bb88252b97305f021a
marsdenmadoka <marsmadoka98@gmail.com>
2024-01-25 22:05:36 +0300

    added new features

---
+++ app/src/main/java/com/example/newsapp/Presentation/onboarding/OnBoardingEvent.kt
@@ -0,0 +1,5 @@
+package com.example.newsapp.Presentation.onboarding
+
+sealed class OnBoardingEvent {
+    object SaveAppEntry:OnBoardingEvent()
+}
--- app/src/main/java/com/example/newsapp/Presentation/onboarding/OnBoardingViewModel.kt
+++ app/src/main/java/com/example/newsapp/Presentation/onboarding/OnBoardingViewModel.kt
@@ -1,4 +1,31 @@
 package com.example.newsapp.Presentation.onboarding
 
-class OnBoardingViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.newsapp.domain.usecases.AppEntryUseCases
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class OnBoardingViewModel @Inject constructor(
+    private val appEntryUseCases: AppEntryUseCases
+):ViewModel(){
+
+    fun onEvent(event:OnBoardingEvent){ //combination between mvi and mvvm to using our viewModels direct in  our screen || we just expose the event to the outside
+        when(event){
+            is OnBoardingEvent.SaveAppEntry ->{
+                saveAppEntry()
+            }
+        }
+    }
+
+    private fun saveAppEntry(){
+        viewModelScope.launch {
+            appEntryUseCases.saveAppEntry()
+        }
+
+    }
+
 }
