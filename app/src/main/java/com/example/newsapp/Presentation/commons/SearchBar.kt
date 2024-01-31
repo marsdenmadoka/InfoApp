c6bf2d894521ab4eb988dca0e3edd54f08867dbd
marsdenmadoka <marsmadoka98@gmail.com>
2024-01-31 19:57:03 +0300

    added new files

--- app/src/main/java/com/example/newsapp/Presentation/commons/SearchBar.kt
+++ app/src/main/java/com/example/newsapp/Presentation/commons/SearchBar.kt
@@ -1,4 +1,75 @@
 package com.example.newsapp.Presentation.commons
 
-class SearchBar {
+import androidx.compose.foundation.border
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier,
+    text: String,
+    readOnly: Boolean,
+    onClick: (() -> Unit)? = null,
+    onValueChange: (String) -> Unit,
+    onSearch: () -> Unit
+) {
+
+    val interactionSource = remember {
+        MutableInteractionSource()
+    }
+
+    val isClicked = interactionSource.collectIsPressedAsState().value
+    LaunchedEffect(key1 = isClicked) {
+        if (isClicked) {
+            onClick?.invoke()
+        }
+    }
+
+    Box(modifier = modifier) {
+        TextField(
+            value = text,
+            onValueChange = onValueChange,
+            modifier = Modifier
+                .fillMaxWidth()
+                .mysearchBarBorder()
+        )
+    }
+
 }
+
+fun Modifier.mysearchBarBorder() = composed {//extension function
+    if(!isSystemInDarkTheme()){
+        border(
+            width = 1.dp,
+            color = Color.Black,
+            shape = MaterialTheme.shapes.medium
+        )
+    } else {
+        this
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
