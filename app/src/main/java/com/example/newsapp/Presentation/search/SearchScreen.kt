10f462aa07d037fc824ed013f6a2dd9a6a4df10c
marsdenmadoka <marsmadoka98@gmail.com>
2024-02-01 11:11:36 +0300

    adding the search screen

--- app/src/main/java/com/example/newsapp/Presentation/search/SearchScreen.kt
+++ app/src/main/java/com/example/newsapp/Presentation/search/SearchScreen.kt
@@ -1,4 +1,43 @@
 package com.example.newsapp.Presentation.search
 
-class SearchScreen {
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.example.newsapp.Presentation.Dimes.MediumPadding1
+import com.example.newsapp.Presentation.Navgraph.Route
+import com.example.newsapp.Presentation.commons.ArticlesList
+import com.example.newsapp.Presentation.commons.SearchBar
+
+@Composable
+fun SearchScreen(
+    state: SearchState,
+    event: (SearchEvent) -> Unit,
+    navigate: (String) -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .padding(
+                top = MediumPadding1,
+                start = MediumPadding1,
+                end = MediumPadding1
+            )
+            .statusBarsPadding()
+    ) {
+        SearchBar(
+            text = state.searchQuery,
+            readOnly = false,
+            onValueChange = { event(SearchEvent.UpdateSearchQuery(it)) },
+            onSearch = { event(SearchEvent.SearchNews) })
+
+        Spacer(modifier = Modifier.height(MediumPadding1))
+        state.articles?.let {
+            val articles = it.collectAsLazyPagingItems()
+            ArticlesList(articles = articles, onClick = {navigate(Route.DetailsScreen.route)})
+        }
+    }
 }
