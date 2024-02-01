17bc7d22ff90f24fef53c308f90a9af77ad11745
marsdenmadoka <marsmadoka98@gmail.com>
2024-02-01 10:21:20 +0300

    adding new search funcs

--- app/src/main/java/com/example/newsapp/data/remote/NewsApi.kt
+++ app/src/main/java/com/example/newsapp/data/remote/NewsApi.kt
@@ -9,12 +9,24 @@ interface NewsApi {
 
     @GET("everything")
     suspend fun getNews(
-//since we are using paging three library
+   //since we are using paging three library
         @Query("page") page: Int,
         @Query("sources") sources: String,
         @Query("apiKey") apiKey:String = API_KEY
     ): NewsResponse
+
+    //for our search functionality
+    @GET("everything")
+    suspend fun searchNews(
+        @Query("q") searchQuery: String,
+        @Query("page") page: Int,
+        @Query("sources") sources: String,
+        @Query("apiKey") apiKey:String = API_KEY
+    ): NewsResponse
+
+
 }
+
 
 
 
--- app/src/main/java/com/example/newsapp/domain/repository/NewsRepository.kt
+++ app/src/main/java/com/example/newsapp/domain/repository/NewsRepository.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.flow.Flow
 
 interface NewsRepository {
     fun getNews(sources:List<String>): Flow<PagingData<Article>>
+    fun searchNews(searchQuery:String, sources:List<String>): Flow<PagingData<Article>>
 
 
 }
