2367b10fa86fadf2496d63753282497627c65c73
marsdenmadoka <marsmadoka98@gmail.com>
2024-02-01 17:26:17 +0300

    adding screen navigations

---
+++ app/src/main/java/com/example/newsapp/Presentation/details/DetailsEvent.kt
@@ -0,0 +1,5 @@
+package com.example.newsapp.Presentation.details
+
+sealed class DetailsEvent {
+    object SaveArticle: DetailsEvent()
+}
--- app/src/main/java/com/example/newsapp/Presentation/details/DetailsScreen.kt
+++ app/src/main/java/com/example/newsapp/Presentation/details/DetailsScreen.kt
@@ -1,4 +1,54 @@
 package com.example.newsapp.Presentation.details
 
-class DetailsScreen {
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.example.newsapp.Presentation.details.components.DetailsTopBar
+import com.example.newsapp.domain.model.Article
+
+@Composable
+fun DetailsScreen(
+    article: Article,
+    event: (DetailsEvent) -> Unit,
+    navigateUp: () -> Unit
+) {
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+    ) {
+
+        DetailsTopBar(
+            onBrowsingClick = {
+                Intent(Intent.ACTION_VIEW).also {
+                    it.data = Uri.parse(article.url)
+                    if (it.resolveActivity(context.packageManager) != null) {
+                        context.startActivity(it)
+                    }
+                }
+            },
+            onShareClick = {
+                Intent(Intent.ACTION_SEND).also {
+                    it.putExtra(Intent.EXTRA_TEXT, article.url)
+                    it.type = "text/plain"
+                    if (it.resolveActivity(context.packageManager) != null) {
+                        context.startActivity(it)
+                    }
+                }
+            },
+            onBookmarkClick = { event(DetailsEvent.SaveArticle)},
+            onBackClick =  navigateUp)
+
+
+    }
+
 }
+
+
