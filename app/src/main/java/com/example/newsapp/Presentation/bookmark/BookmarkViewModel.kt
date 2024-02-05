e1312c3b230437533a60565137c50e528305b841
marsdenmadoka <marsmadoka98@gmail.com>
2024-02-05 23:40:24 +0300

    made new changes

--- app/src/main/java/com/example/newsapp/Presentation/bookmark/BookmarkViewModel.kt
+++ app/src/main/java/com/example/newsapp/Presentation/bookmark/BookmarkViewModel.kt
@@ -3,8 +3,10 @@ package com.example.newsapp.Presentation.bookmark
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.newsapp.domain.usecases.news.NewsUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
@@ -16,9 +18,10 @@ class BookmarkViewModel @Inject constructor(
     private val _state = mutableStateOf(BookmarkState())
     val state: State<BookmarkState> = _state
 
+    //fetching all bookmarked articles from our roomDB
     private fun getArticles(){
         newsUseCases.selectArticles().onEach {
             _state.value = _state.value.copy(articles = it)
-        }
+        }.launchIn(viewModelScope)
     }
 }
