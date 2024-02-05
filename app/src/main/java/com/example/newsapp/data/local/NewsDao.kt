fe10c7fb38b46596cc61de861680c06c0cb44ba8
marsdenmadoka <marsmadoka98@gmail.com>
2024-02-05 22:28:55 +0300

    made new changes

--- app/src/main/java/com/example/newsapp/data/local/NewsDao.kt
+++ app/src/main/java/com/example/newsapp/data/local/NewsDao.kt
@@ -1,7 +1,22 @@
 package com.example.newsapp.data.local
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.newsapp.domain.model.Article
+import kotlinx.coroutines.flow.Flow
 
 @Dao
-class NewsDao {
+interface NewsDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article: Article)
+
+    @Delete
+    suspend fun delete(article: Article)
+
+    @Query("SELECT * FROM ARTICLE")
+    fun getArticles(): Flow<List<Article>>
 }
--- app/src/main/java/com/example/newsapp/domain/model/Article.kt
+++ app/src/main/java/com/example/newsapp/domain/model/Article.kt
@@ -8,7 +8,7 @@ data class Article(
     val content: String,
     val description: String,
     val publishedAt: String,
-    val source: Source,
+  val source: Source,// We can only save primitive data types in room and not objects therefore we have to convert this in object using type converters
     val title: String,
     val url: String,
     val urlToImage: String
