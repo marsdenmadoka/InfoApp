cba3ccdb993e959168cb487a7f153b4c42ceaa1a
marsdenmadoka <marsmadoka98@gmail.com>
2024-02-09 20:04:50 +0300

    added bottom nav

--- app/src/main/java/com/example/newsapp/Presentation/news_navigator/components/NewsBottomNavigation.kt
+++ app/src/main/java/com/example/newsapp/Presentation/news_navigator/components/NewsBottomNavigation.kt
@@ -1,5 +1,6 @@
 package com.example.newsapp.Presentation.news_navigator.components
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -10,14 +11,19 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.newsapp.Presentation.Dimes.ExtraSmallpadding2
 import com.example.newsapp.Presentation.Dimes.IconSize
+import com.example.newsapp.R
+import com.example.newsapp.ui.theme.NewsAppTheme
 
 //Bottom Navigation
 @Composable
@@ -44,11 +50,21 @@ fun NewsBottomNavigation(
                             modifier = Modifier.size(IconSize)
                         )
                         Spacer(modifier = Modifier.height(ExtraSmallpadding2))
-                        Text(text = item.text,
-                            style = MaterialTheme.typography.labelSmall)
+                        Text(
+                            text = item.text,
+                            style = MaterialTheme.typography.labelSmall
+                        )
 
                     }
-                })
+                },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = MaterialTheme.colorScheme.primary,
+                    selectedTextColor = MaterialTheme.colorScheme.primary,
+                    unselectedIconColor = colorResource(id = R.color.body),
+                    unselectedTextColor = colorResource(id = R.color.body),
+                    indicatorColor = MaterialTheme.colorScheme.background
+                )
+            )
 
         }
     }
@@ -57,5 +73,29 @@ fun NewsBottomNavigation(
 data class BottomNavigationItem(
     @DrawableRes val icon: Int,
     val text: String
-
 )
+
+@Preview
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun NewsBottomNavigationPreview(){
+    NewsAppTheme {
+        NewsBottomNavigation(
+            items = listOf(
+                BottomNavigationItem(icon=R.drawable.ic_home, text = "Home"),
+                BottomNavigationItem(icon=R.drawable.ic_search, text = "Search"),
+                BottomNavigationItem(icon=R.drawable.ic_bookmark, text = "Bookmark"),
+
+            ) ,
+            selected =0 ,
+            onItemClick = {})
+    }
+}
+
+
+
+
+
+
+
+
