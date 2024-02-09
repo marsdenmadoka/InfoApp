70710e5d3cbaa7c7c41db90d72a4ec19446c0479
marsdenmadoka <marsmadoka98@gmail.com>
2024-02-09 19:50:23 +0300

    made new changes

--- app/src/main/java/com/example/newsapp/Presentation/news_navigator/components/NewsBottomNavigation.kt
+++ app/src/main/java/com/example/newsapp/Presentation/news_navigator/components/NewsBottomNavigation.kt
@@ -1,4 +1,46 @@
 package com.example.newsapp.Presentation.news_navigator.components
 
-class NewsBottomNavigation {
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+//Bottom Navigation
+@Composable
+fun NewsBottomNavigation(
+    items: List<BottomNavigationItem>,
+    selected: Int,
+    onItemClick: (Int) -> Unit
+) {
+    NavigationBar(
+        modifier = Modifier.fillMaxWidth(),
+        containerColor = MaterialTheme.colorScheme.background,
+        tonalElevation = 10.dp
+    ) {
+
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(
+                selected = index == selected,
+                onClick = { /*TODO*/ },
+                icon = {
+                    Column(horizontalAlignment = CenterHorizontally) {
+
+
+                    }
+                })
+
+        }
+    }
 }
+
+data class BottomNavigationItem(
+    @DrawableRes val icon: Int,
+    val text: String
+
+)
