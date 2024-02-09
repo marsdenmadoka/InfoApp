22b2a78810b0e09edbb91e35c69f5b9d0abcc55e
marsdenmadoka <marsmadoka98@gmail.com>
2024-02-09 19:55:27 +0300

    made new changes

--- app/src/main/java/com/example/newsapp/Presentation/news_navigator/components/NewsBottomNavigation.kt
+++ app/src/main/java/com/example/newsapp/Presentation/news_navigator/components/NewsBottomNavigation.kt
@@ -2,14 +2,22 @@ package com.example.newsapp.Presentation.news_navigator.components
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.newsapp.Presentation.Dimes.ExtraSmallpadding2
+import com.example.newsapp.Presentation.Dimes.IconSize
 
 //Bottom Navigation
 @Composable
@@ -27,10 +35,17 @@ fun NewsBottomNavigation(
         items.forEachIndexed { index, item ->
             NavigationBarItem(
                 selected = index == selected,
-                onClick = { /*TODO*/ },
+                onClick = { onItemClick(index) },
                 icon = {
                     Column(horizontalAlignment = CenterHorizontally) {
-
+                        Icon(
+                            painter = painterResource(id = item.icon),
+                            contentDescription = null,
+                            modifier = Modifier.size(IconSize)
+                        )
+                        Spacer(modifier = Modifier.height(ExtraSmallpadding2))
+                        Text(text = item.text,
+                            style = MaterialTheme.typography.labelSmall)
 
                     }
                 })
